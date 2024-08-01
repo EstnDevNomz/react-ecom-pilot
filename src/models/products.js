e7302367b3e7d80d56902c202b2207c2eca7e698
Lee Hyunseok <exorsa1525@gmail.com>
2024-08-01 19:16:44 +0900

    Feat: 프로젝트 세팅: 프로젝트 구조, Vite 마이그레이싱

---
+++ src/models/products.js
@@ -0,0 +1,41 @@
+// Shoes
+
+// T-shirt
+
+// Trousers
+
+// Tracksuit
+
+// Jacket
+
+// Hat
+
+// Socks
+
+// Gloves
+
+// Underwear
+
+// Swimsuit
+
+// Blouse
+
+// Shirt
+
+// Jeans
+
+// Shorts
+
+// Hoodie
+
+// Jumper
+
+// Bikini
+
+// Trunks
+
+// Slippers
+
+// Sandals
+
+// Boots
